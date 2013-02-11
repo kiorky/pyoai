9ef47e165c3475f7ea4f2331e2916253ce93087e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-02-11 20:19:42 +0100

    allow identifier to be latin1 encoded to avoid charset issues

--- src/oaipmh/client.py
+++ src/oaipmh/client.py
@@ -318,24 +318,73 @@ class Client(BaseClient):
             self._base_url, data=urlencode(kw), headers=headers)
         return retrieveFromUrlWaiting(request)
 
+def magicstring(string):
+    """Convert any string to UTF-8 ENCODED one"""
+    seek = False
+    if isinstance(string, unicode):
+        try:
+            string = string.encode('utf-8')
+        except:
+            seek = True
+    if seek:
+        try:
+            detectedenc = chardet.detect(string).get('encoding')
+        except Exception, e:
+            detectedenc = None
+        if detectedenc:
+            sdetectedenc = detectedenc.lower()
+        else:
+            sdetectedenc = ''
+        if sdetectedenc.startswith('iso-8859'):
+            detectedenc = 'ISO-8859-15'
+
+        found_encodings = [
+            'ISO-8859-15', 'TIS-620', 'EUC-KR',
+            'EUC-JP', 'SHIFT_JIS', 'GB2312', 'utf-8', 'ascii',
+        ]
+        if sdetectedenc not in ('utf-8', 'ascii'):
+            try:
+                if not isinstance(string, unicode):
+                    string = string.decode(detectedenc)
+                string = string.encode(detectedenc)
+            except:
+                for idx, i in enumerate(found_encodings):
+                    try:
+                        if not isinstance(string, unicode) and detectedenc:
+                            string = string.decode(detectedenc)
+                        string = string.encode(i)
+                        break
+                    except:
+                        if idx == (len(found_encodings) - 1):
+                            raise
+    if isinstance(string, unicode):
+        string = string.encode('utf-8')
+    string = string.decode('utf-8').encode('utf-8')
+    return string
+
+
+
 def buildHeader(header_node, namespaces):
     e = etree.XPathEvaluator(header_node, 
                             namespaces=namespaces).evaluate
     identifier = e('string(oai:identifier/text())')
     datestamp = datestamp_to_datetime(
         str(e('string(oai:datestamp/text())')))
-    setspec = [str(s) for s in e('oai:setSpec/text()')]
+    setspec = [magicstring(s) for s in e('oai:setSpec/text()')]
     deleted = e("@status = 'deleted'") 
     return common.Header(identifier, datestamp, setspec, deleted)
 
 def ResumptionListGenerator(firstBatch, nextBatch):
     result, token = firstBatch()
     while 1:
+        prev_token = token
         for item in result:
             yield item
         if token is None:
             break
         result, token = nextBatch(token)
+        if not result and (prev_token == token):
+            break
 
 def retrieveFromUrlWaiting(request,
                            wait_max=WAIT_MAX, wait_default=WAIT_DEFAULT):
